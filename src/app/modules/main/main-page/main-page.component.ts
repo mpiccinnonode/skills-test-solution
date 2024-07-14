fcd5ecd69022adb1ddc408ff1e243307c262dcb9
MaxPiccinno <m.piccinno@node.coop>
2024-07-14 12:00:32 +0200

    navbar

--- src/app/modules/main/main-page/main-page.component.ts
+++ src/app/modules/main/main-page/main-page.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { MenuItem, PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  navItems: MenuItem[] = [
+    {
+      icon: PrimeIcons.CAR,
+      label: 'Cars',
+      routerLink: 'cars',
+    },
+    {
+      icon: PrimeIcons.USERS,
+      label: 'People',
+      routerLink: 'people',
+    },
+  ];
+}
---
+++ src/app/modules/shared/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MenubarModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  items = input<MenuItem[]>([]);
+}
