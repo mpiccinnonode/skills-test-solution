d89265461b6001dfbbdd133ca027083623f75e93
MaxPiccinno <m.piccinno@node.coop>
2024-07-14 11:06:57 +0200

    basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+  {
+    path: 'main',
+    loadChildren: () =>
+      import('./modules/main/main.routes').then((m) => m.mainRoutes),
+  },
+];
---
+++ src/app/modules/main/main.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+
+export const mainRoutes: Route[] = [
+  {
+    path: '',
+    component: MainPageComponent,
+  },
+];
