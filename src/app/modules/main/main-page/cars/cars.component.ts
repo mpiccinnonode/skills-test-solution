02efcdf2ccf46d7a0acb8e39900942af16be6588
MaxPiccinno <m.piccinno@node.coop>
2024-07-14 12:14:18 +0200

    tables

--- src/app/modules/main/main-page/cars/cars.component.ts
+++ src/app/modules/main/main-page/cars/cars.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { CarsService } from '../../../../core/services/cars.service';
+import { Car } from '../../../../core/models/car.model';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-cars',
   standalone: true,
-  imports: [],
+  imports: [TableModule],
   templateUrl: './cars.component.html',
-  styleUrl: './cars.component.scss'
+  styleUrl: './cars.component.scss',
 })
 export class CarsComponent {
+  cars = computed<Car[]>(() => this.carsService.cars());
 
+  constructor(private carsService: CarsService) {}
 }
--- src/app/modules/main/main-page/people/people.component.ts
+++ src/app/modules/main/main-page/people/people.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { PeopleService } from '../../../../core/services/people.service';
+import { Person } from '../../../../core/models/person.model';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [],
+  imports: [TableModule],
   templateUrl: './people.component.html',
-  styleUrl: './people.component.scss'
+  styleUrl: './people.component.scss',
 })
 export class PeopleComponent {
+  people = computed<Person[]>(() => this.peopleService.people());
 
+  constructor(private peopleService: PeopleService) {}
 }
